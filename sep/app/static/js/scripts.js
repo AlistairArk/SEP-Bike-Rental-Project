401a1984bd45321a6110772e3c8a963a2f244e0e
sc17cf <sc17cf@leeds.ac.uk>
2019-02-11 20:06:14 +0000

    Adding my files and changes to the flask app

---
+++ sep/app/static/js/scripts.js
@@ -0,0 +1,21 @@
+
+(function($) {
+    "use strict";
+
+    /*================================
+    Preloader
+    ==================================*/
+
+    var preloader = $('#preloader');
+    $(window).on('load', function() {
+        preloader.fadeOut('slow', function() { $(this).remove(); });
+    });
+
+    /*================================
+    sidebar collapsing
+    ==================================*/
+    $('.nav-btn').on('click', function() {
+        $('.page-container').toggleClass('sbar_collapsed');
+    });
+
+})(jQuery);
--- sep/app/views.py
+++ sep/app/views.py
@@ -1,5 +1,28 @@
 from app import app
+from flask import render_template, redirect, url_for, flash, request, jsonify, session
+
+"""
+#Check if user is needed to be logged in for a page:
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        return redirect(url_for('login'))
+    return decorated_function
+
+#Check is already logged through sessions:
+def login_present(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'logged_in' in session:
+            return redirect(url_for('dashboard'))
+        return f(*args, **kwargs)
+    return decorated_function
+
+"""
 
 @app.route('/')
 def index():
-    return "Hello!!!!!!!!!"
+    #Just rendering login as a test
+    return render_template("staffLogin.html")
