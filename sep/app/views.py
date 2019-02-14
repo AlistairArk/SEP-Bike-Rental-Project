c6449bd45233bfafde982d725534c72073dcb216
Alistair J.H. Donnell <sc17jhd@leeds.ac.uk>
2019-02-14 23:18:41 +0000

    Added website log in function

--- sep/app/views.py
+++ sep/app/views.py
@@ -27,6 +27,31 @@ def index():
     #Just rendering login as a test
     return render_template("staffLogin.html")
 
+@app.route('/resetPassword')
+def resetPassword():
+    return render_template("resetPassword.html")
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = str(request.form['username']) # Takes input username from webpage 
+    password = str(request.form['password']) # Takes input password from webpage   
+    
+    log("Login Test")
+    log("Username: "+ username)
+    log("Password: "+ password+"\n")
+
+    # # Hand username and password to login function
+    validLogin = 1 # Assume valid login as a test
+    
+
+    if validLogin:
+        session["log_in_fail"] = False
+        session["logged_in"] = True
+        return render_template("index.html")
+
+    else:
+        session["log_in_fail"] = True
+        return render_template("resetPassword.html")
 
 # Logging function used for testing
 import datetime
