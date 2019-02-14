57077ac4d13aa07503b7267be413acb14c055ae2
Christopher Feneley <sc17cf@leeds.ac.uk>
2019-02-14 14:15:48 +0000

    Forgot password and Registration UI design complete. Navigation between activities also complete

--- Android/LeedsRide/app/src/main/java/com/leedsride/rentalapp/LeedsRide/MainActivity.java
+++ Android/LeedsRide/app/src/main/java/com/leedsride/rentalapp/LeedsRide/MainActivity.java
@@ -1,7 +1,10 @@
 package com.leedsride.rentalapp.LeedsRide;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button registerSubmitBtn = (Button)findViewById(R.id.registerSubmitBtn);
+        registerSubmitBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent startRegisterIntent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(startRegisterIntent);
+            }
+        });
+
+        Button forgotPasswordBtn = (Button)findViewById(R.id.forgotPasswordBtn);
+        forgotPasswordBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent startFPIntent = new Intent(getApplicationContext(), ForgotPasswordActivity.class);
+                startActivity(startFPIntent);
+            }
+        });
     }
 }
