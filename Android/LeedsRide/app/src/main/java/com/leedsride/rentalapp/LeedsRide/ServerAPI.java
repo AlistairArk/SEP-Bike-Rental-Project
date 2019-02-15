2fdc53d96d329f95044633aca56db4402ffb1a75
Christopher Feneley <sc17cf@leeds.ac.uk>
2019-02-15 01:00:06 +0000

    Started server API

--- Android/LeedsRide/app/src/main/java/com/leedsride/rentalapp/LeedsRide/ServerAPI.java
+++ Android/LeedsRide/app/src/main/java/com/leedsride/rentalapp/LeedsRide/ServerAPI.java
@@ -1,4 +1,32 @@
 package com.leedsride.rentalapp.LeedsRide;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
 public class ServerAPI {
+
+    private URL url;
+    private HttpsURLConnection connection;
+
+    public ServerAPI() throws IOException, MalformedURLException{
+        /* Establish http connection with the server */
+
+        url = new URL("http://www.sc17gs.pythonanywhere.com");
+        connection = (HttpsURLConnection)url.openConnection();
+    }
+
+    public void ServerLogin(String username, String password) {
+
+
+    }
+
+    public void ServerLogout() {
+
+    }
 }
