9d6c9dcc39049b5d79ec8cf3fa7b3078e93876d9
Christopher Feneley <sc17cf@leeds.ac.uk>
2019-02-18 18:33:19 +0000

    Adding new activities and possible migration to androidX

---
+++ Android/LeedsRide/app/src/main/java/com/leedsride/rentalapp/LeedsRide/OnBookingActivity.java
@@ -0,0 +1,13 @@
+package com.leedsride.rentalapp.LeedsRide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OnBookingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_booking);
+    }
+}
