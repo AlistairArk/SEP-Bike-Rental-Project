170da5621983598d3e5c86ca4c24823bcec99fe8
Sara Matib <saramatib@Saras-Air.lan>
2019-02-21 18:24:50 +0000

    added user type to db models

--- sep/app/models.py
+++ sep/app/models.py
@@ -13,6 +13,7 @@ class User(UserMixin,db.Model):
     password = db.Column(db.String(80), nullable=False)
     image = db.Column(db.String(75), nullable=False, default='default_icon')
     phone = db.Column(db.String(20), nullable=False)
+    user_type = db.Column(db.String(20), nullable=False) 
 
     cards = db.relationship('Card', backref='user', lazy=True)
     bookings = db.relationship('Booking', backref='user', lazy=True)
