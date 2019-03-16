e769b5a8242e2a1754b1f4fc2dd37e48419a32ad
Gabbie <sc17gs@leeds.ac.uk>
2019-03-16 17:05:04 +0000

    adding Length validator to forms

--- sep/app/forms.py
+++ sep/app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import Form
 from wtforms import TextAreaField, StringField, BooleanField, validators, IntegerField, SelectField, FloatField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 class addBikesForm(Form):
     amount = IntegerField('amount', [validators.NumberRange(max=10,min=-5)])
@@ -8,7 +8,7 @@ class addBikesForm(Form):
 
 class addLocationForm(Form):
     name = StringField('name', validators=[DataRequired(), Length(min=1,max=50)])
-    addr = TextAreaField('addr', validators=[DataRequired(), Lenght(min=10,max=80)])
+    addr = TextAreaField('addr', validators=[DataRequired(), Length(min=10,max=80)])
     max_capacity = IntegerField('max_capacity', [validators.NumberRange(max=200,min=5)])
     longt = FloatField('longt', validators=[DataRequired()])
     latt = FloatField('latt', validators=[DataRequired()])
