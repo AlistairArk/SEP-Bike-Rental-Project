c2129c526f926010f10258a7eb96cd2d002fcdf2
Anna Rita Mage <sc17arm@leeds.ac.uk>
2019-03-28 23:26:30 +0000

    created form class for add user

--- sep/app/forms.py
+++ sep/app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import Form
-from wtforms import TextAreaField, StringField, BooleanField, validators, IntegerField, SelectField, FloatField
-from wtforms.validators import DataRequired, Length
+from wtforms import TextAreaField, StringField, BooleanField, validators, IntegerField, SelectField, FloatField, PasswordField
+from wtforms.validators import DataRequired, Length, Email
 
 class addBikesForm(Form):
     amount = IntegerField('amount', [validators.NumberRange(max=10,min=-5)])
@@ -12,3 +12,10 @@ class addLocationForm(Form):
     max_capacity = IntegerField('max_capacity', [validators.NumberRange(max=200,min=5)])
     longt = FloatField('longt', validators=[DataRequired()])
     latt = FloatField('latt', validators=[DataRequired()])
+
+class addUserForm(Form):
+    name = StringField('name', validators=[DataRequired(), Length(min=1,max=50)])
+    username = StringField('username', validators=[DataRequired(), Length(min=1,max=50)])
+    email = StringField('email', validators=[DataRequired(), Email()])
+    phone = IntegerField('phone', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=2, max=30)])
