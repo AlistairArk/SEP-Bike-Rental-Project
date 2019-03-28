bff839d2eb5bd102ad9a75a73ab5a8e9d27be07c
Anna Rita Mage <sc17arm@leeds.ac.uk>
2019-03-28 12:21:35 +0000

    new booking form created

--- sep/app/forms.py
+++ sep/app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import Form
-from wtforms import TextAreaField, StringField, BooleanField, validators, IntegerField, SelectField, FloatField
+from wtforms import TextAreaField, StringField, BooleanField, validators, IntegerField, SelectField, FloatField, DateField
 from wtforms.validators import DataRequired, Length
+from wtforms_components import TimeField
 
 class addBikesForm(Form):
     amount = IntegerField('amount', [validators.NumberRange(max=10,min=-5)])
@@ -12,3 +13,12 @@ class addLocationForm(Form):
     max_capacity = IntegerField('max_capacity', [validators.NumberRange(max=200,min=5)])
     longt = FloatField('longt', validators=[DataRequired()])
     latt = FloatField('latt', validators=[DataRequired()])
+
+class addBooking(Form):
+    email = StringField('email', validators=[DataRequired(), Email()])
+    phone = IntegerField('phone', validators=[DataRequired())
+    date = DateField('date', validators=[DataRequired()])
+    stime = TimeField('stime', validators=[DataRequired()])
+    etime = TimeField('stime', validators=[DataRequired()])
+    slocation = StringField('slocation', validators=[DataRequired(), Length(min=1,max=50)])
+    elocation = StringField('elocation', validators=[DataRequired(), Length(min=1,max=50)])
