b29f3e1f541d23d2c8ba60ff4b0eceabb60dc861
sc17jhd <sc17jhd@leeds.ac.uk>
2019-03-28 23:19:44 -1200

    fixed views auto merge bugs

--- sep/app/views.py
+++ sep/app/views.py
@@ -28,8 +28,11 @@ def index():
     # Just rendering login as a test
     return render_template("staffLogin.html")
 
+@app.route('/')
+def index():
+    # Just rendering login as a test
+    return render_template("staffLogin.html")
 
-### ### ###
 
 @app.route('/resetPassword')
 def webResetPassword():
@@ -89,7 +92,6 @@ def webLogout():
     return render_template("staffLogin.html")
 
 ### ### ###
-
 
 @app.route('/addUser',methods=['GET','POST'])
 def addUser():
@@ -226,119 +228,9 @@ def locationAdded():
         return render_template('locationAdded.html',
                                 name=name)
 
-<<<<<<< HEAD
-@app.route('/resetPassword')
-def webResetPassword():
-    return render_template("resetPassword.html")
-
-
-@app.route('/resetRequest', methods=['POST'])
-def webResetRequest():
-    # Send a password reset email to user
-    email = str(request.form['email'])
-
-    if function.resetRequest(email=email):
-        message = "A reset link has been sent to to the provided email."
-        return render_template("resetPassword.html", success = message)
-    else:
-        message = "Invalid email provided. Please try again."
-        return render_template("resetPassword.html", fail = message)
-
-
-@app.route('/login', methods=['POST'])
-def webLogin():
-    # Hand username and password to login function
-    # Return 1 if valid login is found
-    username = str(request.form['username'])
-    password = str(request.form['password'])
-
-    loginData = function.login(username=username, password=password)
-
-
-    if loginData[0]:
-        session["userType"] = loginData[1]
-        session["username"] = loginData[2]
-        session["name"] = loginData[3]
-        session["loggedIn"] = True
-        return webIndex()
-    else:
-        session["loggedIn"] = False
-        message = "Error: The User Name or Password entered is incorrect. Please try again."
-        return render_template("staffLogin.html", message = message)
-
-
-@app.route('/index')
-def webIndex():
-    if session["loggedIn"]:
-        return render_template("index.html", name = session["name"])
-    else:
-        message = "Error: You must be logged in to peform that action."
-        return render_template("staffLogin.html", name = message)
-
-
-@app.route('/logout')
-def webLogout():
-    session["loggedIn"] = False
-    session["userType"] = None
-    session["username"] = None
-    session["name"] = None
-    return render_template("staffLogin.html")
-
-
-@app.route('/addEmployeeForm',methods=['GET','POST'])
-def webAddEmployeeForm():
-    return render_template("addEmployee.html")
-
-
-
-
-
-@app.route('/addEmployee', methods=['POST'])
-def webAddEmployee():
-    # Take employee details from add employee form and add the new employee
-
-    name      = str(request.form['name'])
-    username  = str(request.form['username'])
-    email     = str(request.form['email'])
-    password  = str(request.form['password'])
-    phone     = str(request.form['phone'])
-
-
-    # Call add employee function
-    result = function.addUser(name=name, username=username, email=email, password=password, phone=phone, userType="employee" )
-
-    message = "Error: The User Name or Password entered is incorrect. Please try again."
-    return render_template("staffLogin.html", message = message)
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # Logging function used for testing
-# import datetime
-# logging = 0
-# def log(*args):
-#     if logging:
-#         for line in args:
-#             if isinstance(line, str):
-#                 with open('log.log', 'a') as the_file:
-#                     time = f"{datetime.datetime.now():%Y/%m/%d - %H:%M:%S}"
-#                     the_file.write("\n["+str(time)+"] "+line)
-#             elif debug:
-#                 raise line
-=======
 @app.route('/locationStats')
 def locationStats():
     locations = models.Location.query.all()
     # locations = [[1,'leeds','123 house',5],[2,'headingley','44 drive',6],[3,'burley','77 street',9]]
     return render_template('locationStats.html',
                             locations=locations)
->>>>>>> e5eea521269950af35e335ebd8ebfc8f14c539ce
