5873a25805eee1b1f7ada62756f4452725111a81
Khalil Ibrahim <khalil@beytna.net>
2019-04-16 14:41:07 -0500

    retrofit API interface added

---
+++ Android/LeedsRide/app/src/main/java/com/leedsride/rentalapp/LeedsRide/models/Login.java
@@ -0,0 +1,47 @@
+package com.leedsride.rentalapp.LeedsRide.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Login {
+
+    /*********************
+     * Declared fields are only used internally,
+     * what needs to match the API key names are the Serialized names
+     */
+    @SerializedName("username")
+    @Expose
+    private String username;
+    @SerializedName("password")
+    @Expose
+    private String password;
+    @SerializedName("loginStatus")
+    @Expose
+    private String loginStatus;
+
+    /// only basic constructor and a couple getters + setters need to be used
+
+    public Login() {
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLoginStatus() {
+        return loginStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "Conversation{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", loginStatus='" + loginStatus + '\'' +
+                '}';
+    }
+}
---
+++ Android/LeedsRide/app/src/main/java/com/leedsride/rentalapp/LeedsRide/restAPI.java
@@ -0,0 +1,14 @@
+package com.leedsride.rentalapp.LeedsRide;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+
+import com.leedsride.rentalapp.LeedsRide.models.Login;
+
+public interface restAPI {
+    @Headers("Content-Type: application/json")
+    @POST("/") ////////////Remainder of url from shortened base url in main activity
+    Call<Login> getReply(@Body Login login); //When POST request is made, the body should be an instance of Conversation class called convo
+}
