3c1257a5eae84623c98646b9ebf80aff6354d4a4
Anna Rita Mage <sc17arm@leeds.ac.uk>
2019-04-23 18:44:55 +0100

    merge new location into booking branch

--- sep/app/views.py
+++ sep/app/views.py
@@ -3,15 +3,51 @@ from flask import render_template, redirect, url_for, flash, request, jsonify, s
 from app import app, models, db
 from .forms import *
 import datetime
+from flaskext.mysql import MySQL
 
 
 @app.route('/newBooking', methods=['GET','POST'])
 def newBooking():
-    # form.slocation.choices=[(l.id,l.name) for l in models.Location.query.all()]
-    # form.elocation.choices=[(l.id,l.name) for l in models.Location.query.all()]
+    form=addBookingForm(request.form)
+
+    form.slocation.choices=[(l.id,l.name) for l in models.Location.query.all()]
+    form.elocation.choices=[(l.id,l.name) for l in models.Location.query.all()]
 
     return render_template("newBooking.html", form=form)
 
+@app.route('/addLocation',methods=['GET','POST'])
+def addLocation():
+    form=addLocationForm(request.form)
+    return render_template('newLocation.html',
+                            form=form)
+
+
+@app.route('/locationAdded',methods=['GET','POST'])
+def locationAdded():
+    if request.method == 'POST':
+        locationInfo = request.form
+        for key,value in locationInfo.items():
+            if key=='name':
+                name=value
+            elif key=='addr':
+                addr=value
+            elif key=='max_capacity':
+                max_capacity=value
+            elif key=='longt':
+                longt=float(value)
+            elif key=='latt':
+                latt=float(value)
+        l = models.Location(name=name,
+                            bike_amount=0,
+                            max_capacity=max_capacity,
+                            addr=addr,
+                            longt=longt,
+                            latt=latt)
+        db.session.add(l)
+        db.session.commit()
+        flash("Location added!")
+        return render_template('locationAdded.html',
+                                name=name)
 
 
 # @app.route('/newBooking',methods=['GET','POST'])
