63f4dea4864c708f47db92f1fc4b37951d8ab9d7
Anna Rita Mage <sc17arm@leeds.ac.uk>
2019-04-23 18:47:55 +0100

    commented out test views

--- sep/app/views.py
+++ sep/app/views.py
@@ -18,8 +18,7 @@ def newBooking():
 @app.route('/addLocation',methods=['GET','POST'])
 def addLocation():
     form=addLocationForm(request.form)
-    return render_template('newLocation.html',
-                            form=form)
+    return render_template('newLocation.html',form=form)
 
 
 @app.route('/locationAdded',methods=['GET','POST'])
@@ -46,8 +45,7 @@ def locationAdded():
         db.session.add(l)
         db.session.commit()
         flash("Location added!")
-        return render_template('locationAdded.html',
-                                name=name)
+        return render_template('locationAdded.html', name=name)
 
 
 # @app.route('/newBooking',methods=['GET','POST'])
@@ -116,37 +114,37 @@ def index():
     #Just rendering index as a test
     return render_template("index.html")
 
-@app.route('/owner')
-def ownerMainPage():
-    #Just rendering reset password as a test
-    return render_template("ownerMainPage.html")
-
-@app.route('/addEmployee')
-def addEmployee():
-    #Just rendering addEmployee as a test
-    return render_template("addEmployee.html")
-
-@app.route('/staffLogin')
-def staffLogin():
-    #Just rendering login as a test
-    return render_template("staffLogin.html")
-
-@app.route('/resetPassword')
-def resetPassword():
-    #Just rendering reset password as a test
-    return render_template("resetPassword.html")
-
-@app.route('/addBikes')
-def addBikes():
-    #Just rendering addBikes as a test
-    return render_template("addBikes.html")
-
-@app.route('/newLocation')
-def newLocation():
-    #Just rendering newLocation as a test
-    return render_template("newLocation.html")
-
-@app.route('/test')
-def test():
-    #Just rendering test as a test
-    return render_template("test.html")
+# @app.route('/owner')
+# def ownerMainPage():
+#     #Just rendering reset password as a test
+#     return render_template("ownerMainPage.html")
+#
+# @app.route('/addEmployee')
+# def addEmployee():
+#     #Just rendering addEmployee as a test
+#     return render_template("addEmployee.html")
+#
+# @app.route('/staffLogin')
+# def staffLogin():
+#     #Just rendering login as a test
+#     return render_template("staffLogin.html")
+#
+# @app.route('/resetPassword')
+# def resetPassword():
+#     #Just rendering reset password as a test
+#     return render_template("resetPassword.html")
+#
+# @app.route('/addBikes')
+# def addBikes():
+#     #Just rendering addBikes as a test
+#     return render_template("addBikes.html")
+#
+# @app.route('/newLocation')
+# def newLocation():
+#     #Just rendering newLocation as a test
+#     return render_template("newLocation.html")
+#
+# @app.route('/test')
+# def test():
+#     #Just rendering test as a test
+#     return render_template("test.html")
